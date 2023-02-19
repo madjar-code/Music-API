e2259f3a6fe195ed12f6fbba29970938f41660ec
Evan <evanmadjar@gmail.com>
2023-02-19 12:41:42 +0300

    I created song and album models after writing test

--- server/music/models.py
+++ server/music/models.py
@@ -8,3 +8,28 @@ class MusicianPerformer(UUIDModel, TimeStampModel, SoftDeletionModel):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Album(UUIDModel, TimeStampModel, SoftDeletionModel):
+    name = models.CharField(max_length=255)
+    year_of_issue = models.PositiveIntegerField()
+    musician_performer = models.ForeignKey(
+        to=MusicianPerformer,
+        related_name='albums',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return f'{self.name} - {self.year_of_issue} - {self.musician_performer}'
+
+
+class Song(UUIDModel, TimeStampModel, SoftDeletionModel):
+    name = models.CharField(max_length=255)
+    position = models.PositiveIntegerField()
+    album = models.ForeignKey(
+        to=Album, related_name='songs',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return f'{self.name} - {self.album.musician_performer}'
--- server/music/tests/test_models.py
+++ server/music/tests/test_models.py
@@ -1,7 +1,8 @@
 from uuid import UUID
+from datetime import datetime
 from django.test import TestCase
 from music.models import\
-    MusicianPerformer
+    MusicianPerformer, Album, Song
 
 
 class TestMusicianPerformerModel(TestCase):
@@ -15,9 +16,6 @@ class TestMusicianPerformerModel(TestCase):
     
     def test_musician_performer_can_be_created(self) -> None:
         self.assertEqual(str(self.mp), 'Arctic Monkeys')
-    
-    def test_musician_performer_uuid(self) -> None:
-        self.assertEqual(type(self.mp.id), UUID)
     
     def test_soft_deletion(self) -> None:
         self.assertTrue(self.mp.is_active)
@@ -34,4 +32,47 @@ class TestMusicianPerformerModel(TestCase):
         new_mp_objects = MusicianPerformer.active_objects.all()
         self.assertFalse(self.mp2 in new_mp_objects)
 
+
+class TestAlbumModel(TestCase):
+    def setUp(self) -> None:
+        self.mp = MusicianPerformer.objects.create(
+            name='Arctic Monkeys'
+        )
+
+        self.album = Album.objects.create(
+            name='AM',
+            year_of_issue=2012,
+            musician_performer = self.mp
+        )
+
+    def test_album_can_be_created(self) -> None:
+        self.assertEqual(str(self.album), 'AM - 2012 - Arctic Monkeys')
+
+    def test_album_uuid(self) -> None:
+        self.assertEqual(type(self.album.id), UUID)
+
+
+class TestSongModel(TestCase):
+    def setUp(self) -> None:
+        self.mp = MusicianPerformer.objects.create(
+            name='Kaleo'
+        )
     
+        self.album = Album.objects.create(
+            name='A/B',
+            year_of_issue=2016,
+            musician_performer = self.mp
+        )
+
+        self.song = Song.objects.create(
+            name='Automobile',
+            position=7,
+            album=self.album
+        )
+
+    def test_song_can_be_created(self) -> None:
+        self.assertEqual(str(self.song), 'Automobile - Kaleo')
+    
+    def test_timestamp(self) -> None:
+        self.assertEqual(type(self.song.created_at), datetime)
+        self.assertEqual(type(self.song.updated_at), datetime)
